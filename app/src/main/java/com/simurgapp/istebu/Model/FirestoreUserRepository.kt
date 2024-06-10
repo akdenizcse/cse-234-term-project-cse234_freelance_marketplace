96212fbfdb7bf4560d61eea869ebee59df06d98a
Zehra03 <120973755+Zehra03@users.noreply.github.com>
2024-06-11 00:59:09 +0300

    Update FirestoreUserRepository.kt

--- app/src/main/java/com/simurgapp/istebu/Model/FirestoreUserRepository.kt
+++ app/src/main/java/com/simurgapp/istebu/Model/FirestoreUserRepository.kt
@@ -12,35 +12,50 @@ class FirestoreUserRepository {
     val UID = "123456789"
 
 
-    fun add(name: String , surname: String , country: String , city: String , email: String ,
-            phoneNumber: String , education: String , careerFields: MutableList<String>, definition: String , imageURL: String  = "", dailyPrice: Int ){
-        val freelancer = hashMapOf(
-            "name" to name,
-            "UID" to UID,
-            "surname" to surname,
-            "country" to country,
-            "city" to city,
-            "email" to email,
-            "phoneNumber" to phoneNumber,
-            "education" to education,
-            "careerFields" to careerFields,
-            "definition" to definition,
-            "imageURL" to imageURL,
-            "dailyPrice" to dailyPrice,
-            "email" to email,
-            "rating" to 0.0f,
-            "pastProjects" to mutableListOf<String>(),
-            "ongoingProjects" to mutableListOf<String>(),
-            "completedGivenProjects" to mutableListOf<String>(),
-            "ongoingGivenProjects" to mutableListOf<String>(),
-            "reviews" to mutableListOf<String>(),
-            "comments" to mutableListOf<String>()
+    fun addFreelancer(education: String , careerFields: MutableList<String>, definition: String , imageURL: String  = "", dailyPrice: Int ){
 
-        )
-        db.collection("Freelancers").document(UID)
-            .set(freelancer)
-            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
-            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+        getUserByUID(UID, { document ->
+            val name = document.getString("name") ?: ""
+            val surname = document.getString("surname") ?: ""
+            val country = document.getString("country") ?: ""
+            val city = document.getString("city") ?: ""
+            val email = document.getString("email") ?: ""
+            val phoneNumber = document.getString("phoneNumber") ?: ""
+            val job = document.getString("job") ?: ""
+
+
+
+            val freelancer = hashMapOf(
+                "name" to name,
+                "UID" to UID,
+                "job" to job,
+                "surname" to surname,
+                "country" to country,
+                "city" to city,
+                "email" to email,
+                "phoneNumber" to phoneNumber,
+                "education" to education,
+                "careerFields" to careerFields,
+                "definition" to definition,
+                "imageURL" to imageURL,
+                "dailyPrice" to dailyPrice,
+                "rating" to 0.0f,
+                "pastProjects" to mutableListOf<String>(),
+                "ongoingProjects" to mutableListOf<String>(),
+                "completedGivenProjects" to mutableListOf<String>(),
+                "ongoingGivenProjects" to mutableListOf<String>(),
+                "reviews" to mutableListOf<String>(),
+                "comments" to mutableListOf<String>()
+
+            )
+            db.collection("Freelancers").document(UID)
+                .set(freelancer)
+                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+        }, { exception ->
+            Log.w(TAG, "Error getting user document", exception)
+        })
+
     }
     fun addCommentsWithUID(UID: String, comment: String){
         db.collection("Freelancers").document(UID)
@@ -62,6 +77,37 @@ class FirestoreUserRepository {
                 Log.d(TAG, "get failed with ", exception)
             }
     }
-
-
+    fun addUserBYUID(UID: String, name: String , surname: String , country: String , city: String , email: String ,
+                     phoneNumber: String , job: String){
+        val user = hashMapOf(
+            "name" to name,
+            "UID" to UID,
+            "surname" to surname,
+            "country" to country,
+            "city" to city,
+            "email" to email,
+            "phoneNumber" to phoneNumber,
+            "job" to job,
+            "completedGivenProjects" to mutableListOf<String>(),
+            "ongoingGivenProjects" to mutableListOf<String>()
+        )
+        db.collection("Users").document(UID)
+            .set(user)
+            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+    }
+    fun getUserByUID(UID: String, onSuccess: (DocumentSnapshot) -> Unit, onFailure: (Exception) -> Unit) {
+        db.collection("Users").document(UID).get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    onSuccess(document)
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                onFailure(exception)
+                Log.d(TAG, "get failed with ", exception)
+            }
+    }
 }
