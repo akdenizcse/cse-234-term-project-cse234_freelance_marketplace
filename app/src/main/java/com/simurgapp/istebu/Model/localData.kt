67a0783a39c4f989084b37a606a5fea90161235f
Zehra Bozkurt <zehrabzk@gmail.com>
2024-06-11 19:28:25 +0300

    localdata

--- app/src/main/java/com/simurgapp/istebu/Model/localData.kt
+++ app/src/main/java/com/simurgapp/istebu/Model/localData.kt
@@ -12,12 +12,30 @@ class SharedPreferencesHelper(context: Context) {
     fun getUID(): String? {
         return sharedPreferences.getString("uid", null)
     }
+    fun saveEmail(email: String) {
+        sharedPreferences.edit().putString("email", email).apply()
+    }
+    fun getEmail(): String? {
+        return sharedPreferences.getString("email", null)
+    }
 
     fun saveLoginState(isLoggedIn: Boolean) {
         sharedPreferences.edit().putBoolean("is_logged_in", isLoggedIn).apply()
     }
     fun isLoggedIn(): Boolean {
         return sharedPreferences.getBoolean("is_logged_in", false)
+    }
+    fun isUserCreated(): Boolean {
+        return sharedPreferences.getBoolean("is_user_created", false)
+    }
+    fun saveUserCreated(isUserCreated: Boolean) {
+        sharedPreferences.edit().putBoolean("is_user_created", isUserCreated).apply()
+    }
+    fun isFreelancerCreated(): Boolean {
+        return sharedPreferences.getBoolean("is_freelancer_created", false)
+    }
+    fun saveFreelancerCreated(isFreelancerCreated: Boolean) {
+        sharedPreferences.edit().putBoolean("is_freelancer_created", isFreelancerCreated).apply()
     }
 
     fun clear() {
