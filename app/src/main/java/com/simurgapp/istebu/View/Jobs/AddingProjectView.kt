3bf2ba8d1e57d9453f5936f9217536556ffe05c0
Zehra Bozkurt <zehrabzk@gmail.com>
2024-06-27 19:57:16 +0300

    Implement UI for adding new projects in Compose

--- app/src/main/java/com/simurgapp/istebu/View/Jobs/AddingProjectView.kt
+++ app/src/main/java/com/simurgapp/istebu/View/Jobs/AddingProjectView.kt
@@ -1,7 +1,10 @@
 package com.simurgapp.istebu.View.Jobs
 
 
+import android.net.Uri
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -22,8 +25,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.BorderColor
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material.icons.filled.Comment
+import androidx.compose.material.icons.filled.LocalAtm
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -43,15 +48,24 @@ import androidx.navigation.NavController
 import com.simurgapp.istebu.Model.FirestoreUserRepository
 import com.simurgapp.istebu.Model.ProjectClass
 import com.simurgapp.istebu.Model.SharedPreferencesHelper
+import com.simurgapp.istebu.Model.tempData
+import com.simurgapp.istebu.View.RegistrationProcedure.SelectedFieldsView
+import com.simurgapp.istebu.View.UIElements.Counter
+import com.simurgapp.istebu.View.UIElements.DatePicker
+import com.simurgapp.istebu.View.UIElements.DropDown
+import com.simurgapp.istebu.View.UIElements.FilledTonalButton
+import com.simurgapp.istebu.View.UIElements.SelectableDropDown
+import com.simurgapp.istebu.View.UIElements.TextFieldOne
+import com.simurgapp.istebu.ui.theme.Orange200
+import com.simurgapp.istebu.ui.theme.darkerOrange
 import java.util.UUID
 
 @Composable
 fun AddingProjectView(navController: NavController) {
     val projectName = remember { mutableStateOf("") }
-    val skills = remember { mutableStateListOf<String>() }
+    val skills = remember { mutableStateListOf<String>("") }
     val description = remember { mutableStateOf("") }
     val imageURL = remember { mutableStateListOf<String>() }
-    val necessaryBranches = remember { mutableStateListOf<String>() }
     val numberPeople = remember { mutableStateOf("") }
     val budget = remember { mutableStateOf("") }
     val estimatedTime = remember { mutableStateOf("") }
@@ -61,154 +75,102 @@ fun AddingProjectView(navController: NavController) {
     val sharedPreferencesHelper = SharedPreferencesHelper(context)
     val currentUserID = sharedPreferencesHelper.getUID() ?: ""
     val firestoreUserRepository = FirestoreUserRepository()
+    val projectTypeList = listOf("Hourly", "Fixed")
+    val experienceLevelList = listOf("Beginner", "Intermediate", "Advanced")
+    val tempData = tempData()
+    val UID = UUID.randomUUID().toString()
+    val selectedImagesUrl = remember { mutableStateListOf<Uri>() }
+
+    val careerFields = listOf(
+        "Graphic Design",
+        "Programming",
+        "Translation",
+        "Teaching",
+        "Fitness Training",
+        "UI/UX Design",
+        "Video Editing",
+        "Voiceover",
+        "Life Coaching",
+        "Nutrition Coach",
+        "Photoshop",
+        "Illustration",
+        "Animation",
+        "Social Media Management",
+        "Marketing",
+        "Copywriting",
+        "Accountant"
+    )
+
+    val selectedFields = remember { mutableStateListOf<String>() }
+    var subBranches = remember { mutableStateListOf<String>() }
+    val selectedSubBranch = remember { mutableStateListOf<String>() }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.GetMultipleContents()) { uris: List<Uri> ->
+        selectedImagesUrl.clear()
+        selectedImagesUrl.addAll(uris)
+    }
+
     Column(
         modifier = Modifier
             .padding(16.dp, bottom = 66.dp)
             .verticalScroll(rememberScrollState())
-            .fillMaxSize()
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        BasicTextField(
-            value = projectName.value,
-            onValueChange = { projectName.value = it },
-            decorationBox = { innerTextField ->
-                Box(
-                    modifier = Modifier
-                        .background(Color.Gray.copy(alpha = 0.1f))
-                        .padding(8.dp)
-                ) {
-                    if (projectName.value.isEmpty()) {
-                        Text("Project Name", color = Color.Gray)
-                    }
-                    innerTextField()
+        TextFieldOne(labelText = "Project Name", leadingIconOne = Icons.Default.BorderColor , colorOne = Orange200 , colorTwo =  darkerOrange, text = projectName )
+        Spacer(modifier = Modifier.height(8.dp) )
+        TextFieldOne(labelText = "Description", leadingIconOne = Icons.Default.Comment , colorOne = Orange200 , colorTwo =  darkerOrange, text = description )
+        Spacer(modifier = Modifier.height(8.dp) )
+        TextFieldOne(labelText = "Budget", leadingIconOne = Icons.Default.LocalAtm , colorOne = Orange200 , colorTwo =  darkerOrange, text = budget )
+        Spacer(modifier = Modifier.height(8.dp) )
+        DatePicker { estimatedTime.value = it }
+        Spacer(modifier = Modifier.height(8.dp) )
+        DropDown(list = projectTypeList,  onSelected = { projectType.value = projectTypeList[it] })
+        Spacer(modifier = Modifier.height(4.dp) )
+        DropDown(list = experienceLevelList,  onSelected = { experienceLevel.value = experienceLevelList[it] })
+        Spacer(modifier = Modifier.height(8.dp) )
+        SelectableDropDown(
+            careerFields = careerFields,
+            selectedIndices = selectedFields,
+            text = "Career Fields",
+            onItemSelected = { index ->
+                if (selectedFields.contains(careerFields[index])) {
+                    selectedFields.remove(careerFields[index])
+                } else {
+                    selectedFields.add(careerFields[index])
+                    subBranches += tempData.careerFields[index].subList(2, tempData().careerFields[index].size)
                 }
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp)
+            }
         )
+        SelectedFieldsView(selectedFields.sortedBy { str ->
+            str.length
+        }) { field ->
+            selectedFields.remove(field)
+            careerFields.indexOf(field).let {
+                subBranches.removeAll(tempData.careerFields[it].subList(2, tempData().careerFields[it].size))
+                selectedSubBranch.removeAll(tempData.careerFields[it].subList(2, tempData().careerFields[it].size))
+            }
 
-        BasicTextField(
-            value = description.value,
-            onValueChange = { description.value = it },
-            decorationBox = { innerTextField ->
-                Box(
-                    modifier = Modifier
-                        .background(Color.Gray.copy(alpha = 0.1f))
-                        .padding(8.dp)
-                ) {
-                    if (description.value.isEmpty()) {
-                        Text("Description", color = Color.Gray)
-                    }
-                    innerTextField()
-                }
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp)
-        )
-
-
-
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-            BasicTextField(
-                value = budget.value,
-                onValueChange = { budget.value = it },
-                decorationBox = { innerTextField ->
-                    Box(
-                        modifier = Modifier
-                            .background(Color.Gray.copy(alpha = 0.1f))
-                            .padding(8.dp)
-                    ) {
-                        if (budget.value.isEmpty()) {
-                            Text("Budget", color = Color.Gray)
-                        }
-                        innerTextField()
-                    }
-                },
-                modifier = Modifier
-                    .width(150.dp)
-                    .padding(bottom = 8.dp)
-            )
-            BasicTextField(
-                value = estimatedTime.value,
-                onValueChange = { estimatedTime.value = it },
-                decorationBox = { innerTextField ->
-                    Box(
-                        modifier = Modifier
-                            .background(Color.Gray.copy(alpha = 0.1f))
-                            .padding(8.dp)
-                    ) {
-                        if (estimatedTime.value.isEmpty()) {
-                            Text("Estimated Time", color = Color.Gray)
-                        }
-                        innerTextField()
-                    }
-                },
-                modifier = Modifier
-                    .width(150.dp)
-                    .padding(bottom = 8.dp)
-            )
         }
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-
-            BasicTextField(
-                value = projectType.value,
-                onValueChange = { projectType.value = it },
-                decorationBox = { innerTextField ->
-                    Box(
-                        modifier = Modifier
-                            .background(Color.Gray.copy(alpha = 0.1f))
-                            .padding(8.dp)
-                    ) {
-                        if (projectType.value.isEmpty()) {
-                            Text("Project Type", color = Color.Gray)
-                        }
-                        innerTextField()
-                    }
-                },
-                modifier = Modifier
-                    .width(150.dp)
-                    .padding(bottom = 8.dp)
-            )
-            BasicTextField(
-                value = experienceLevel.value,
-                onValueChange = { experienceLevel.value = it },
-                decorationBox = { innerTextField ->
-                    Box(
-                        modifier = Modifier
-                            .background(Color.Gray.copy(alpha = 0.1f))
-                            .padding(8.dp)
-                    ) {
-                        if (experienceLevel.value.isEmpty()) {
-                            Text("Experience Level", color = Color.Gray)
-                        }
-                        innerTextField()
-                    }
-                },
-                modifier = Modifier
-                    .width(150.dp)
-                    .padding(bottom = 8.dp)
-            )
+        Spacer(modifier =Modifier.height(4.dp))
+        SelectableDropDown(careerFields = subBranches , selectedIndices = selectedSubBranch,"Sub Branch") { index ->
+            if (selectedSubBranch.contains(subBranches[index])) {
+                selectedSubBranch.remove(subBranches[index])
+            } else {
+                selectedSubBranch.add(subBranches[index])
+            }
         }
-            BasicTextField(
-                value = numberPeople.value,
-                onValueChange = { numberPeople.value = it },
-                decorationBox = { innerTextField ->
-                    Box(
-                        modifier = Modifier
-                            .background(Color.Gray.copy(alpha = 0.1f))
-                            .padding(8.dp)
-                    ) {
-                        if (numberPeople.value.isEmpty()) {
-                            Text("Number of People", color = Color.Gray)
-                        }
-                        innerTextField()
-                    }
-                },
-                modifier = Modifier
-                    .width(150.dp)
-                    .padding(bottom = 8.dp)
-            )
+        Spacer(modifier =Modifier.height(4.dp))
+
+        SelectedFieldsView(selectedFields = selectedSubBranch.sortedBy { str ->
+            str.length
+        }){ field ->
+            selectedSubBranch.remove(field)
+
+        }
+        Counter ({ numberPeople.value = it.toString() },"Number of People" )
+
+
 
 
 
@@ -217,6 +179,7 @@ fun AddingProjectView(navController: NavController) {
             Text(text = "Skills", fontSize = 18.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 8.dp))
             skills.forEachIndexed { index, skill ->
                 Row() {
+
                     BasicTextField(
                         value = skill,
                         onValueChange = { skills[index] = it },
@@ -247,90 +210,30 @@ fun AddingProjectView(navController: NavController) {
         }
 
 
-        Column {
-            Text(text = "Necessary Branches", fontSize = 18.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 8.dp))
-            necessaryBranches.forEachIndexed { index, branch ->
-                Row() {
-                    BasicTextField(
-                        value = branch,
-                        onValueChange = { necessaryBranches[index] = it },
-                        decorationBox = { innerTextField ->
-                            Box(
-                                modifier = Modifier
-                                    .background(Color.Gray.copy(alpha = 0.1f))
-                                    .padding(8.dp)
-                            ) {
-                                if (branch.isEmpty()) {
-                                    Text("Branch", color = Color.Gray)
-                                }
-                                innerTextField()
-                            }
-                        },
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(bottom = 8.dp)
-                    )
-                    IconButton(onClick = { necessaryBranches.removeAt(index) }) {
-                        Icon(Icons.Default.Close, contentDescription = "Remove Branch")
-                    }
-                }
-            }
-            IconButton(onClick = { necessaryBranches.add("") }) {
-                Icon(Icons.Default.Add, contentDescription = "Add Branch")
-            }
-        }
 
 
-        Column {
-            Text(text = "Images", fontSize = 18.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 8.dp))
-            imageURL.forEachIndexed { index, url ->
-                Row() {
-                    BasicTextField(
-                        value = url,
-                        onValueChange = { imageURL[index] = it },
-                        decorationBox = { innerTextField ->
-                            Box(
-                                modifier = Modifier
-                                    .background(Color.Gray.copy(alpha = 0.1f))
-                                    .padding(8.dp)
-                            ) {
-                                if (url.isEmpty()) {
-                                    Text("Image URL", color = Color.Gray)
-                                }
-                                innerTextField()
-                            }
-                        },
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(bottom = 8.dp)
-                    )
-                    IconButton(onClick = { imageURL.removeAt(index) }) {
-                        Icon(Icons.Default.Close, contentDescription = "Remove Image")
-                    }
-                }
-            }
-            IconButton(onClick = { imageURL.add("") }) {
-                Icon(Icons.Default.Add, contentDescription = "Add Image")
-            }
-        }
+
+        FilledTonalButton(onClick = {
+            launcher.launch("image/*")
+        }, text = "Add Image")
 
         Spacer(modifier = Modifier.height(16.dp))
         Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center ){
             com.simurgapp.istebu.View.UIElements.FilledTonalButton(onClick = {
                 println("basıldııııı")
                 val project = ProjectClass(
-                    UID = UUID.randomUUID().toString(),
+                    UID = UID,
                     projectName = projectName.value,
                     clientID = currentUserID,
                     skills = skills.toMutableList(),
                     description = description.value,
                     date = System.currentTimeMillis(),
-                    imageURL = imageURL.toMutableList(),
-                    necessaryBranches = necessaryBranches.toMutableList(),
+                    imageURL = (selectedImagesUrl.map { it.toString() }).toMutableList(),
+                    necessaryBranches = selectedSubBranch.toMutableList(),
                     numberPeople = numberPeople.value.toIntOrNull() ?: 0,
                     budget = budget.value.toFloatOrNull() ?: 0f,
                     isFinished = false,
-                    estimatedTime = estimatedTime.value.toIntOrNull() ?: 0,
+                    estimatedTime = estimatedTime.value,
                     projectType = projectType.value,
                     experienceLevel = experienceLevel.value)
                 firestoreUserRepository.addProject(project,{
